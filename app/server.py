f2c9c410a22208309448518222955db933e454bc
emanuele orlandin <42842129+lelus78@users.noreply.github.com>
2025-06-15 22:32:32 +0200

    Fix mask predictor coordinate

--- app/server.py
+++ app/server.py
@@ -149,7 +149,7 @@ def make_mask(pil_img, parts_to_mask, conf_threshold=0.20):
     sam_predictor.set_image(np.array(pil_img.convert("RGB")))
     
     masks, scores, _ = sam_predictor.predict(
-        point_coords=np.array(point_coords),
+        point_coords=center_point,
         point_labels=np.array(point_labels),
         multimask_output=True,
     )
