2cec773ecf2c498c7b0d91c46d8d4c19b54ed5d3
emanuele orlandin <42842129+lelus78@users.noreply.github.com>
2025-06-16 15:35:33 +0200

    Fix layout duplication and streamline initialization

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,17 +1,12 @@
 import { assignDomElements, dom } from './state.js';
-import { initTheme } from './theme.js';
 import { setupEventListeners, resetWorkflow, closeModal } from './workflow.js';
 import { loadStickers } from './stickers.js';
 import { animationLoop } from './memeEditor.js';
-import { loadGallery, setupGalleryInteraction, initSidebarToggle } from './gallery.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   assignDomElements();
-  initTheme(dom.themeToggle, dom.themeIcon);
-  initSidebarToggle(dom.sidebar, dom.sidebarToggle, dom.galleryToggle, dom.galleryContainer);
   setupEventListeners();
   loadStickers();
-  loadGallery(dom.galleryContainer).then(() => setupGalleryInteraction(dom.galleryContainer));
   resetWorkflow();
   animationLoop();
   window.closeModal = closeModal;
